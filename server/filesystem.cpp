f2c78ddd03f3af9bc0339f486b4393ecfb7e6ffa
Raymond W. Ko <raymond.w.ko@gmail.com>
2015-12-19 03:24:20 -0500

    use machine ID to decrypt key, add expiration date

--- server/filesystem.cpp
+++ server/filesystem.cpp
@@ -41,21 +41,27 @@ Filesystem::~Filesystem() {
   CloseHandle(mFileHandle);
 }
 
-void Filesystem::decrypt(const std::string& ciphertext,
-                         std::string& plaintext) {
+void Filesystem::decrypt(const std::string& ciphertext, const std::string& key,
+                         std::string& plaintext, bool uncompress) {
   std::string compressed_plaintext;
 
-  CryptoPP::AES::Decryption aes_decryption(&mKey[0], mKey.size());
+  CryptoPP::AES::Decryption aes_decryption((byte*)&key[0], key.size());
   CryptoPP::CBC_Mode_ExternalCipher::Decryption cbc_decryption(aes_decryption,
                                                                &mIV[0]);
+  auto string_sink = new CryptoPP::StringSink(compressed_plaintext);
   CryptoPP::StreamTransformationFilter stf_decryptor(
-      cbc_decryption, new CryptoPP::StringSink(compressed_plaintext));
+      cbc_decryption, string_sink);
   stf_decryptor.Put(reinterpret_cast<const unsigned char*>(ciphertext.c_str()),
                     ciphertext.size());
   stf_decryptor.MessageEnd();
+  /* delete string_sink; */
 
-  snappy::Uncompress(compressed_plaintext.c_str(), compressed_plaintext.size(),
-                     &plaintext);
+  if (uncompress) {
+    snappy::Uncompress(compressed_plaintext.c_str(),
+                       compressed_plaintext.size(), &plaintext);
+  } else {
+    plaintext = compressed_plaintext;
+  }
 }
 
 void Filesystem::load_pack(std::wstring pack_path) {
@@ -75,30 +81,56 @@ void Filesystem::load_pack(std::wstring pack_path) {
   if (mMapHandle == NULL) {
     error("could not create file mapping for enigma file pack");
   }
-  mBytes = (const byte*) MapViewOfFile(mMapHandle, FILE_MAP_READ, 0, 0, 0);
+  mBytes = (const byte*)MapViewOfFile(mMapHandle, FILE_MAP_READ, 0, 0, 0);
   if (!mBytes) {
     auto err = GetLastError();
     error("could not MapViewOfFile of enigma file pack");
   }
 }
 
+bool Filesystem::exists(std::string path) {
+  if (mPathToBytes.size() == 0) return false;
+  return mPathToBytes.count(path) > 0;
+}
+
+std::string Filesystem::get_file(std::string path) {
+  if (!this->exists(path)) error("path in file pack does not exist: " + path);
+
+  const byte* p = mPathToBytes[path];
+  size_t n = mPathToEncryptedSize[path];
+  std::string ciphertext((const char*)p, n);
+  std::string plaintext;
+  this->decrypt(ciphertext, mKey, plaintext);
+  return plaintext;
+}
+
+std::vector<std::string> Filesystem::get_file_list() {
+  std::vector<std::string> files;
+  for (auto iter : mPathToBytes) {
+    files.push_back(iter.first);
+  }
+  return files;
+}
+
 void Filesystem::load_key(std::string path) {
-  std::string keystr = read_file(path);
-  memcpy(&mKey[0], &keystr[0], CryptoPP::AES::DEFAULT_KEYLENGTH);
-      
-  static const char* magic = "SYENIGMA";
+  std::string encrypted_key = read_file(path);
+  std::string keystr = decrypt_key(encrypted_key);
+  mKey = keystr;
 
   mPathToBytes.clear();
   mPathToEncryptedSize.clear();
 
   const byte* p = mBytes;
+  static const char* magic = "SYENIGMA";
   if (memcmp(magic, p, 8))
-    error("file pack does not start with magic 'SYENIGMA'");
+    error(
+        "File pack has been corrupted! It does not start with magic "
+        "'SYENIGMA'");
   p += 8;
   size_t encrypted_header_length = read_size_t(p);
   std::string encrypted_header((const char*)p, encrypted_header_length);
   std::string headerstr;
-  this->decrypt(encrypted_header, headerstr);
+  this->decrypt(encrypted_header, mKey, headerstr);
   const byte* header = (const byte*)&headerstr[0];
 
   p += encrypted_header_length;
@@ -116,28 +148,29 @@ void Filesystem::load_key(std::string path) {
     mPathToEncryptedSize[path] = encrypted_file_size;
     /* std::cout << path << " " << encrypted_file_size << "\n"; */
   }
-}
-
-bool Filesystem::exists(std::string path) {
-  if (mPathToBytes.size() == 0) return false;
-  return mPathToBytes.count(path) > 0;
-}
-
-std::string Filesystem::get_file(std::string path) {
-  if (!this->exists(path)) error("path in file pack does not exist: " + path);
-
-  const byte* p = mPathToBytes[path];
-  size_t n = mPathToEncryptedSize[path];
-  std::string ciphertext((const char*)p, n);
-  std::string plaintext;
-  this->decrypt(ciphertext, plaintext);
-  return plaintext;
-}
-
-std::vector<std::string> Filesystem::get_file_list() {
-  std::vector<std::string> files;
-  for (auto iter : mPathToBytes) {
-    files.push_back(iter.first);
+  
+  std::string datestr = this->get_file("/EXPIRATION_DATE");
+  boost::algorithm::trim_all(datestr);
+  std::stringstream ss;
+  ss << datestr;
+  std::time_t timestamp;
+  ss >> timestamp;
+  std::time_t current_time = std::time(nullptr);
+  if (current_time > timestamp) {
+    error("Access to this resource has expired.");
   }
-  return files;
+}
+
+std::string Filesystem::decrypt_key(std::string encrypted_key) {
+  std::string machine_id = GetMachineId();
+
+  CryptoPP::SHA256 hash;
+  byte digest[CryptoPP::SHA256::DIGESTSIZE];
+  hash.CalculateDigest(digest, (byte*)machine_id.c_str(), machine_id.length());
+
+  std::string secret((char*)digest, CryptoPP::AES::DEFAULT_KEYLENGTH);
+  /* std::vector<byte> iv(CryptoPP::AES::BLOCKSIZE, '\0'); */
+  std::string key;
+  this->decrypt(encrypted_key, secret, key, false);
+  return key;
 }
--- server/filesystem.hpp
+++ server/filesystem.hpp
@@ -2,24 +2,28 @@ class Filesystem {
  public:
   Filesystem(std::wstring pack_path);
   ~Filesystem();
-  
-  bool exists(std::string path);
+
   void load_key(std::string path);
+
+  bool exists(std::string path);
   std::string get_file(std::string path);
-  void decrypt(const std::string& ciphertext, std::string& plaintext);
+
+  void decrypt(const std::string& ciphertext, const std::string& key,
+               std::string& plaintext, bool uncompress = true);
   std::vector<std::string> get_file_list();
 
  private:
   void load_pack(std::wstring pack_path);
-  
+  std::string decrypt_key(std::string encrypted_key);
+
   HANDLE mFileHandle;
   HANDLE mMapHandle;
   const byte* mBytes;
   size_t mSize;
-  
-  std::vector<byte> mKey;
+
+  std::string mKey;
   std::vector<byte> mIV;
-  
+
   const byte* mFileBytes;
   std::unordered_map<std::string, const byte*> mPathToBytes;
   std::unordered_map<std::string, size_t> mPathToEncryptedSize;
