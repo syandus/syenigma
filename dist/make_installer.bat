06c6d451b9d716604f78f7825f38cdd1e1a02b90
Raymond W. Ko <raymond.w.ko@gmail.com>
2015-12-21 01:21:16 -0500

    add installer files

---
+++ dist/make_installer.bat
@@ -0,0 +1,3 @@
+cls
+"C:\Program Files (x86)\WiX Toolset v3.10\bin\candle.exe" -ext WixUIExtension -ext WixUtilExtension merck_courses_english_v1.wxs
+"C:\Program Files (x86)\WiX Toolset v3.10\bin\light.exe" -ext WixUIExtension -ext WixUtilExtension merck_courses_english_v1.wixobj
