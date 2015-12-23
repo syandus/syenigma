59125067c2f672da11a1f43e2375627096bd4178
Raymond W. Ko <raymond.w.ko@gmail.com>
2015-12-23 14:37:34 -0500

    MSI installer tweaks
    modified license agreement according to spec
    custom dialog art according to spec
    custom text strings according to spec

--- dist/make_installer.bat
+++ dist/make_installer.bat
@@ -1,3 +1,3 @@
 cls
 "C:\Program Files (x86)\WiX Toolset v3.10\bin\candle.exe" -ext WixUIExtension -ext WixUtilExtension merck_courses_english_v1.wxs
-"C:\Program Files (x86)\WiX Toolset v3.10\bin\light.exe" -ext WixUIExtension -ext WixUtilExtension merck_courses_english_v1.wixobj
+"C:\Program Files (x86)\WiX Toolset v3.10\bin\light.exe" -ext WixUIExtension -ext WixUtilExtension -cultures:en-us -loc en-us.wxl merck_courses_english_v1.wixobj
